c1cc4b974cb257d92c6919c08d2231d2a0104a01
Felipe Assis <felipeassis97@gmail.com>
2024-01-09 10:52:54 -0300

    feat: initial structure and home

---
+++ Vollmed/Views/ScheduleAppointmentView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleAppointmentView.swift
+//  Vollmed
+//
+//  Created by Felipe Assis on 09/01/24.
+//
+
+import SwiftUI
+
+struct ScheduleAppointmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleAppointmentView()
+}
